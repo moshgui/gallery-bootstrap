55e022b0416a77bf6b01946c9b766d195bc2d806
moshgui <guilherme14_pvh@hotmail.com>
2022-08-14 18:24:03 -0400

    finished

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//Arquivos sass
+import './scss/index.scss';
+
+//Dependencias
+import 'jquery';
+import 'bootstrap';
+
+//Arquivos js
+import './js/core/includes';
+
+import './js/plugins/cityButtons';
